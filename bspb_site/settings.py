a157b700653b43e6edb5662ed2aff29a7f7042db
Isusik <otlivtokenovich@gmail.com>
2025-08-02 16:05:57 +0200

    use absolute db path

--- bspb_site/settings.py
+++ bspb_site/settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-DATA_DIR = BASE_DIR / 'data'
+DATA_DIR = Path('/root/db')
 STATEMENTS_DIR = BASE_DIR / 'statements'
 DATA_DIR.mkdir(parents=True, exist_ok=True)
 STATEMENTS_DIR.mkdir(parents=True, exist_ok=True)
