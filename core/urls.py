b45dbecb52fb17d46eff06a24ac1e465b45d4387
Isusik <otlivtokenovich@gmail.com>
2025-08-05 17:27:43 +0200

    Add migration and template for user panel

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('panel/', views.panel, name='panel'),
     path('statements', views.list_statements_meta, name='statements_list'),
     path('statement/custom', views.statement_custom, name='statement_custom'),
     path('statement/<int:statement_id>', views.statement_meta, name='statement_meta'),
